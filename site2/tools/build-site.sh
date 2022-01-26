92593881f50c6c636f78cc4e4a549336b8a6dc98
LiLi <urfree@apache.org>
2022-01-26 16:31:23 +0800

    feat: just build the changed versions
    Signed-off-by: LiLi <urfree@apache.org>

--- site2/tools/build-site.sh
+++ site2/tools/build-site.sh
@@ -102,7 +102,7 @@ if [ -n "$NEXT" ]; then
   node scripts/split-swagger-by-version.js
   # Because there are too many versions of the document, the memory overflows during the full build. 
   # The split-version-build script is used to build in different versions, and finally the build results are merged.
-  bash scripts/split-version-build.sh
+  bash scripts/split-version-build.sh $2
 else
   # crowdin
   yarn build
--- site2/website-next/scripts/split-version-build.sh
+++ site2/website-next/scripts/split-version-build.sh
@@ -4,17 +4,28 @@ node scripts/split-version.js
 
 latest=$(cat scripts/.latest)
 
+echo "changed files: " 
+echo $@
+
 while read version; do
-    echo $version "build begin..."
-    echo "[\"${latest}\", \"${version}\"]" >versions.json
-    yarn build
-    mkdir -p build-${version}/${version} build-${version}/${version}.md
-    cp -r build/docs/${version}/* build-${version}/${version}
-    cp -r build/docs/${version}.md/* build-${version}/${version}.md
-    echo $version "build done..."
+    if [[ $@ == *website-next/versioned_docs/version-$version* ]]
+    then
+        echo $version "has changed, begin rebuild..."
+        echo "[\"${latest}\", \"${version}\"]" >versions.json
+        yarn build
+        mkdir -p build-${version}/${version} build-${version}/${version}.md
+        cp -r build/docs/${version}/* build-${version}/${version}
+        cp -r build/docs/${version}.md/* build-${version}/${version}.md
+        echo $version "build done..."
+    else
+        echo $version "no change, skip"
+    fi
 done <scripts/.versions
 
 while read version; do
-    mv build-$version/* build/docs
-    rm -rf build-$version
+    if [[ $@ == *website-next/versioned_docs/version-$version* ]]
+    then
+        mv build-$version/* build/docs
+        rm -rf build-$version
+    fi
 done <scripts/.versions
