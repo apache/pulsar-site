d7a2eedbddb7b9d6782c5ff4ed8ef15d8fdad47f
LiLi <urfree@apache.org>
2022-02-11 15:19:38 +0800

    update
    Signed-off-by: LiLi <urfree@apache.org>

---
+++ site2/website-next/migrate/migrate-chapter.js
@@ -0,0 +1,117 @@
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const migrateDocs = require("./migrate-docs");
+
+const migrate = (version, category) => {
+  let version_full = "version-" + version;
+  let src = "../../website/versioned_docs/" + version_full;
+  let dest = "../../website-next/versioned_docs/" + version_full;
+  if (version == "next") {
+    src = "../../docs";
+    dest = "../../website-next/docs";
+  }
+  src = path.join(__dirname, src);
+  dest = path.join(__dirname, dest);
+
+  let sidebar_file = path.join(
+    __dirname,
+    "../../website/versioned_sidebars/" + version_full + "-sidebars.json"
+  );
+  if (version == "next") {
+    sidebar_file = path.join(__dirname, "../../website/sidebars.json");
+  }
+  let sidebar = fs.readFileSync(sidebar_file, "utf8");
+  sidebar = JSON.parse(sidebar);
+
+  const _key = version == "next" ? "docs" : version_full + "-docs";
+  sidebar = sidebar[_key][category];
+  if (version != "next") {
+    sidebar = sidebar.map((item) => {
+      return item.substr(version_full.length + 1);
+    });
+  }
+  if (!sidebar) {
+    return;
+  }
+
+  let new_sidebar_file = "";
+  let new_sidebar = {};
+
+  if (version == "next") {
+    new_sidebar_file = path.join(__dirname, "../../website-next/sidebars.json");
+    try {
+      new_sidebar = fs.readFileSync(new_sidebar_file, "utf8");
+      new_sidebar = JSON.parse(new_sidebar);
+    } catch {
+      new_sidebar = {
+        docsSidebar: [],
+      };
+    }
+    let categoryMap = _.keyBy(new_sidebar.docsSidebar, "label");
+    if (!categoryMap[category]) {
+      new_sidebar.docsSidebar.push({
+        type: "category",
+        label: category,
+        items: sidebar,
+      });
+    } else {
+      categoryMap[category].items = sidebar;
+      new_sidebar.docsSidebar = _.values(categoryMap);
+    }
+  } else {
+    new_sidebar_file = path.join(
+      __dirname,
+      "../../website-next/versioned_sidebars/" + version_full + "-sidebars.json"
+    );
+    try {
+      new_sidebar = fs.readFileSync(new_sidebar_file, "utf8");
+      new_sidebar = JSON.parse(new_sidebar);
+    } catch {
+      new_sidebar = {
+        [version_full + "/docsSidebar"]: [],
+      };
+    }
+    let categoryMap = _.keyBy(
+      new_sidebar[version_full + "/docsSidebar"],
+      "label"
+    );
+    if (!categoryMap[category]) {
+      new_sidebar[version_full + "/docsSidebar"].push({
+        type: "category",
+        label: category,
+        items: sidebar.map((item) => {
+          return {
+            type: "doc",
+            id:
+              version_full +
+              "/" +
+              (item == "deploy-docs" ? "deploy-dcos" : item),
+          };
+        }),
+        // collapsible: true,
+        // collapsed: true,
+      });
+    } else {
+      categoryMap[category].items = sidebar.map((item) => {
+        return {
+          type: "doc",
+          id:
+            version_full + "/" + (item == "deploy-docs" ? "deploy-dcos" : item),
+        };
+      });
+      new_sidebar[version_full + "/docsSidebar"] = _.values(categoryMap);
+    }
+  }
+  fs.writeFileSync(new_sidebar_file, JSON.stringify(new_sidebar, null, 2));
+
+  for (let docsId of sidebar) {
+    migrateDocs(version, docsId);
+  }
+};
+
+//Test
+if (typeof require !== "undefined" && require.main === module) {
+  //   migrate("next", "Pulsar IO");
+  migrate("2.8.0", "Pulsar IO");
+}
