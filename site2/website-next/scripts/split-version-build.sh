7f2626449e5b8b70a50332d58e70206191f20b4c
LiLi <urfree@apache.org>
2022-02-25 10:33:51 +0800

    update
    Signed-off-by: LiLi <urfree@apache.org>

--- site2/website-next/scripts/split-version-build.sh
+++ site2/website-next/scripts/split-version-build.sh
@@ -10,24 +10,24 @@ echo $@
 locals=("en" "zh-CN" "zh-TW" "ja" "ko" "fr")
 
 while read version; do
-    # if [[ $@ == *website-next/versioned_docs/version-$version* ]]; then
-    echo $version "has changed, begin rebuild..."
-    echo "[\"${latest}\", \"${version}\"]" >versions.json
-    yarn build
-    mkdir -p build-${version}/${version} build-${version}/${version}.md
-    cp -r build/docs/${version}/* build-${version}/${version}
-    cp -r build/docs/${version}.md/* build-${version}/${version}.md
-    for language in ${locals[@]}; do
-        if [ -d "build/${language}/docs/${version}" ]; then
-            mkdir -p build-${language}-${version}/${version} build-${language}-${version}/${version}.md
-            cp -r build/${language}/docs/${version}/* build-${language}-${version}/${version}
-            cp -r build/${language}/docs/${version}.md/* build-${language}-${version}/${version}.md
-        fi
-    done
-    echo $version "build done..."
-    # else
-    #     echo $version "no change, skip"
-    # fi
+    if [[ $@ == *website-next/versioned_docs/version-$version* ]]; then
+        echo $version "has changed, begin rebuild..."
+        echo "[\"${latest}\", \"${version}\"]" >versions.json
+        yarn build
+        mkdir -p build-${version}/${version} build-${version}/${version}.md
+        cp -r build/docs/${version}/* build-${version}/${version}
+        cp -r build/docs/${version}.md/* build-${version}/${version}.md
+        for language in ${locals[@]}; do
+            if [ -d "build/${language}/docs/${version}" ]; then
+                mkdir -p build-${language}-${version}/${version} build-${language}-${version}/${version}.md
+                cp -r build/${language}/docs/${version}/* build-${language}-${version}/${version}
+                cp -r build/${language}/docs/${version}.md/* build-${language}-${version}/${version}.md
+            fi
+        done
+        echo $version "build done..."
+    else
+        echo $version "no change, skip"
+    fi
 done <scripts/.versions
 
 echo "latest version begin build..."
@@ -36,15 +36,15 @@ yarn build
 echo "latest version build done..."
 
 while read version; do
-    # if [[ $@ == *website-next/versioned_docs/version-$version* ]]; then
-    mv build-$version/* build/docs
-    rm -rf build-$version
+    if [[ $@ == *website-next/versioned_docs/version-$version* ]]; then
+        mv build-$version/* build/docs
+        rm -rf build-$version
 
-    for language in ${locals[@]}; do
-        if [ -d "build-$language-$version" ]; then
-            mv build-$language-$version/* build/$language/docs
-            rm -rf build-$language-$version
-        fi
-    done
-    # fi
+        for language in ${locals[@]}; do
+            if [ -d "build-$language-$version" ]; then
+                mv build-$language-$version/* build/$language/docs
+                rm -rf build-$language-$version
+            fi
+        done
+    fi
 done <scripts/.versions
