bc158f28247a725773451b82cc0249c3144fb5c1
LiLi <urfree@apache.org>
2022-03-04 15:12:28 +0800

    update
    Signed-off-by: LiLi <urfree@apache.org>

--- site2/tools/docker-build-site.sh
+++ site2/tools/docker-build-site.sh
@@ -24,7 +24,7 @@
 set -e
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
-# cd $ROOT_DIR/pulsar-client-cpp
+cd $ROOT_DIR/pulsar-client-cpp
 
 BUILD_IMAGE_NAME="${BUILD_IMAGE_NAME:-apachepulsar/pulsar-build}"
 BUILD_IMAGE_VERSION="${BUILD_IMAGE_VERSION:-ubuntu-16.04-pb3-website}"
@@ -44,8 +44,8 @@ CROWDIN_DOCUSAURUS_API_KEY=${CROWDIN_DOCUSAURUS_API_KEY:-UNSET}
 
 DOCKER_CMD="docker run -i -e CI_USER=$CI_USER -e CI_GROUP=$CI_GROUP -v $HOME/.m2:/root/.m2 -e CROWDIN_DOCUSAURUS_PROJECT_ID=${CROWDIN_DOCUSAURUS_PROJECT_ID} -e CROWDIN_DOCUSAURUS_API_KEY=${CROWDIN_DOCUSAURUS_API_KEY} -v $ROOT_DIR:/pulsar $IMAGE"
 
-# sed -i "s#$ROOT_DIR#/pulsar#g" $ROOT_DIR/distribution/server/target/classpath.txt
-# sed -i "s#$HOME#/root#g" $ROOT_DIR/distribution/server/target/classpath.txt
+sed -i "s#$ROOT_DIR#/pulsar#g" $ROOT_DIR/distribution/server/target/classpath.txt
+sed -i "s#$HOME#/root#g" $ROOT_DIR/distribution/server/target/classpath.txt
 
 CMD="export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 && cd /pulsar && /pulsar/site2/tools/build-site.sh $@"
 echo "docker exec cmd: "$CMD
