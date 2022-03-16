591458b4cfe8603d937455aa7ce3385c8a79c416
LiLi <urfree@apache.org>
2022-03-16 16:46:23 +0800

    fix: image missing in docs/cookbooks-retention-expiry.md
    Signed-off-by: LiLi <urfree@apache.org>

--- site2/website-next/scripts/release-notes.sh
+++ site2/website-next/scripts/release-notes.sh
@@ -1,19 +1,18 @@
 #! /bin/sh
-# echo -e "\n\nY\n" | ./release-notes.sh
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 TOKEN=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
 
 echo $TOKEN >./token
-# gh auth login --with-token <token
+gh auth login --with-token <token
 cd $ROOT_DIR/../pulsar
 
 mkdir -p $ROOT_DIR/site2/website-next/scripts/release-notes
 
 VERSIONS=$(cat $ROOT_DIR/site2/website-next/versions-full.json | jq -r ".[]")
-# for version in $VERSIONS; do
-#     gh pr list --limit 200 --label "release/"$version --state merged --json labels,title,number > $ROOT_DIR/site2/website-next/scripts/release-notes/$version.json
-# done
+for version in $VERSIONS; do
+    gh pr list --limit 200 --label "release/"$version --state merged --json labels,title,number > $ROOT_DIR/site2/website-next/scripts/release-notes/$version.json
+done
 
 node $ROOT_DIR/site2/website-next/scripts/release-notes.js $ROOT_DIR
