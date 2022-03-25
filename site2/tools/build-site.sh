602a76715b75e4d13f9fa3218d62967cd9d6c268
LiLi <urfree@apache.org>
2022-03-25 12:28:17 +0800

    feat: crowdin actions optimize
    Signed-off-by: LiLi <urfree@apache.org>

--- site2/tools/build-site.sh
+++ site2/tools/build-site.sh
@@ -102,16 +102,19 @@ yarn
 if [ -n "$NEXT" ]; then
   yarn write-translations
   CURRENT_HOUR=$(date +%H)
-  echo "------ crowdin envs:" $CROWDIN_UPLOAD $CROWDIN_DOWNLOAD $CURRENT_HOUR
-  if [[ "$CROWDIN_UPLOAD" == "1" || $CURRENT_HOUR -lt 6 ]]; then
-    echo "------ crowdin upload"
+  echo "------ crowdin envs:" "CROWDIN_UPLOAD: "$CROWDIN_UPLOAD "CROWDIN_DOWNLOAD: "$CROWDIN_DOWNLOAD "CURRENT_HOUR: "$CURRENT_HOUR
+  if [[ "$CROWDIN_UPLOAD" == "1" || $CURRENT_HOUR -eq 6 ]]; then
+    echo "------ exec crowdin upload"
     yarn run crowdin-upload
+  else 
+    echo "------ skip crowdin upload"
   fi
-  if [[ "$CROWDIN_DOWNLOAD" == "1" || $CURRENT_HOUR -gt 12 ]]; then
-    echo "------ crowdin download"
+  if [[ "$CROWDIN_DOWNLOAD" == "1" || $CURRENT_HOUR -eq 12 ]]; then
+    echo "------ exec crowdin download"
     yarn crowdin-download
     echo 'all' > scripts/.language
   else
+    echo "------ skip crowdin download"
     echo 'en' > scripts/.language
   fi
 
