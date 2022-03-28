02d46e6a6de29fe565e904010bb8cb1e4280e72b
Yan Zhang <streamnativeyan@163.com>
2022-03-28 15:17:55 +0800

    Update the path

---
+++ site2/website-next/release-notes/docs/pulsar-c#-1.1.1.md
@@ -0,0 +1,12 @@
+---
+id: pulsar-c#-1.1.1
+title: Pulsar DotPulsar
+sidebar_label: Pulsar DotPulsar
+---
+ 
+
+## Fixed
+
+- The producer ignored message metadata provided by the user
+
+
--- site2/website-next/scripts/release-notes.sh
+++ site2/website-next/scripts/release-notes.sh
@@ -2,17 +2,18 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-echo $TOKEN >$ROOT_DIR/site2/website-next/scripts/token
-gh auth login --with-token <$ROOT_DIR/site2/website-next/scripts/token
-rm $ROOT_DIR/site2/website-next/scripts/token
+# echo $TOKEN >$ROOT_DIR/site2/website-next/scripts/token
+# gh auth login --with-token <$ROOT_DIR/site2/website-next/scripts/token
+# rm $ROOT_DIR/site2/website-next/scripts/token
 
-cd $ROOT_DIR/../pulsar
+# cd $ROOT_DIR/../pulsar
 
-mkdir -p $ROOT_DIR/site2/website-next/scripts/release-notes
+# mkdir -p $ROOT_DIR/site2/website-next/scripts/release-notes
 
-VERSIONS=$(cat $ROOT_DIR/site2/website-next/versions-full.json | jq -r ".[]")
-for version in $VERSIONS; do
-    gh pr list --limit 200 --label "release/"$version --state merged --json labels,title,number > $ROOT_DIR/site2/website-next/scripts/release-notes/$version.json
-done
+# VERSIONS=$(cat $ROOT_DIR/site2/website-next/versions-full.json | jq -r ".[]")
+# for version in $VERSIONS; do
+#     gh pr list --limit 200 --label "release/"$version --state merged --json labels,title,number > $ROOT_DIR/site2/website-next/scripts/release-notes/$version.json
+# done
 
-node $ROOT_DIR/site2/website-next/scripts/release-notes.js $ROOT_DIR
+# node $ROOT_DIR/site2/website-next/scripts/release-notes.js $ROOT_DIR
+node $ROOT_DIR/site2/website-next/scripts/release-notes-other.js $ROOT_DIR
