31c0dcdb5f51c487f8bf53c60f1eba64cfae2a6d
Li Li <urfree@apache.org>
2022-05-05 19:00:10 +0800

    fix: update build script
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/tools/build-site.sh
+++ site2/tools/build-site.sh
@@ -66,7 +66,7 @@ EOF
 set -x -e
 
 export NODE_OPTIONS="--max-old-space-size=16000"
-${ROOT_DIR}/site2/tools/generate-api-docs.sh
+# ${ROOT_DIR}/site2/tools/generate-api-docs.sh
 cd ${ROOT_DIR}/site2/$WEBSITE_DIR
 
 yarn
@@ -111,11 +111,11 @@ cd ${ROOT_DIR}/site2/$WEBSITE_DIR
 
 rm -rf ${ROOT_DIR}/generated-site/content
 mkdir -p ${ROOT_DIR}/generated-site/content
-cp -R ${ROOT_DIR}/generated-site/api ${ROOT_DIR}/generated-site/content
+# cp -R ${ROOT_DIR}/generated-site/api ${ROOT_DIR}/generated-site/content
 if [ -n "$NEXT" ]; then
   cp -R ./build/* ${ROOT_DIR}/generated-site/content
 else
   cp -R ./build/pulsar/* ${ROOT_DIR}/generated-site/content
 fi
 cp -R ${ROOT_DIR}/generated-site/tools ${ROOT_DIR}/generated-site/content
-cp -R ${ROOT_DIR}/site2/$WEBSITE_DIR/static/swagger/* ${ROOT_DIR}/generated-site/content/swagger/
+# cp -R ${ROOT_DIR}/site2/$WEBSITE_DIR/static/swagger/* ${ROOT_DIR}/generated-site/content/swagger/
