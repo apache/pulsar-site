21996ce9a5970f4d35196be1addac4cfc07a0494
Li Li <urfree@apache.org>
2022-05-09 14:51:14 +0800

    sync script optimize
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/website-next/scripts/sync-docs.sh
+++ site2/website-next/scripts/sync-docs.sh
@@ -3,6 +3,7 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $ROOT_DIR/../
 
+# --------------------------------------- Before new website launched ---------------------------------------
 cp -r pulsar/site2/website/blog/* pulsar-site/site2/website/blog
 cp -r pulsar/site2/website/blog/* pulsar-site/site2/website-next/blog
 
@@ -28,14 +29,6 @@ cp -r pulsar/site2/website/pulsar-adapters-release.json pulsar-site/site2/websit
 cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website/static/swagger/
 cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website-next/static/swagger/
 
-# cp -r pulsar/site2/docs/* pulsar-site/site2/website-next/docs
-# cp -r pulsar-site/site2/website-next/docs/assets/* pulsar-site/site2/website-next/static/assets
-# rm -rf pulsar-site/site2/website-next/docs/assets
-# cp -r pulsar/site2/website/versioned_docs/* pulsar-site/site2/website-next/versioned_docs
-# cp -r pulsar/site2/website/versioned_sidebars/* pulsar-site/site2/website-next/versioned_sidebars
-# cp -r pulsar/site2/website/sidebars.json pulsar-site/site2/website-next/sidebars.json
-# cp -r pulsar/site2/website/versions.json pulsar-site/site2/website-next/versions.json
-
 cd pulsar-site/site2/website-next
 if [ -d "node_modules" ]; then
     echo "skip install"
@@ -46,3 +39,18 @@ fi
 node scripts/split-swagger-by-version.js
 node migrate/migrate-blogs.js
 node migrate/migrate-full.js
+
+# --------------------------------------- After new website launched and old website backed up ---------------------------------------
+# cp -r pulsar/site2/blog/* pulsar-site/site2/website-next/blog
+# cp -r pulsar/site2/docs/* pulsar-site/site2/website-next/docs
+# cp -r pulsar-site/site2/website-next/docs/assets/* pulsar-site/site2/website-next/static/assets
+# rm -rf pulsar-site/site2/website-next/docs/assets
+# cp -r pulsar/site2/website/versioned_docs/* pulsar-site/site2/website-next/versioned_docs
+# cp -r pulsar/site2/website/versioned_sidebars/* pulsar-site/site2/website-next/versioned_sidebars
+# cp -r pulsar/site2/website/sidebars.json pulsar-site/site2/website-next/sidebars.json
+# cp -r pulsar/site2/website/versions.json pulsar-site/site2/website-next/versions.json
+# cp -r pulsar/site2/website/releases.json pulsar-site/site2/website-next/releases.json
+# cp -r pulsar/site2/website/pulsar-manager-release-notes.md pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release-notes.md
+# cp -r pulsar/site2/website/pulsar-manager-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release.json
+# cp -r pulsar/site2/website/pulsar-adapters-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-adapters-release.json
+# cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website-next/static/swagger/
