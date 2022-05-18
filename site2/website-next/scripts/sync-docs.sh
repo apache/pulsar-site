44f50c681dc0d6d00f9a751b40db6a944d38802a
Li Li <urfree@apache.org>
2022-05-18 12:18:26 +0800

    fix: change the sync docs scripts
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/website-next/scripts/sync-docs.sh
+++ site2/website-next/scripts/sync-docs.sh
@@ -1,38 +1,28 @@
 #! /bin/sh
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
-cd $ROOT_DIR/../
 
-# --------------------------------------- Before new website launched ---------------------------------------
-cp -r pulsar/site2/website/blog/* pulsar-site/site2/website/blog
-cp -r pulsar/site2/website/blog/* pulsar-site/site2/website-next/blog
+SRC_DIR=$ROOT_DIR/../pulsar/site2
+if [ "" != $1"" ]; then # when preview in pulsar repo
+    SRC_DIR=$ROOT_DIR/../..
+fi
 
-cp -r pulsar/site2/docs/* pulsar-site/site2/docs
-cp -r pulsar/site2/website/sidebars.json pulsar-site/site2/website/sidebars.json
-cp -r pulsar/site2/website/versioned_docs/* pulsar-site/site2/website/versioned_docs
-cp -r pulsar/site2/website/versioned_sidebars/* pulsar-site/site2/website/versioned_sidebars
-cp -r pulsar/site2/docs/assets/* pulsar-site/site2/website-next/static/assets
+cp -r $SRC_DIR/blog/* $ROOT_DIR/site2/website-next/blog
+cp -r $SRC_DIR/docs/* $ROOT_DIR/site2/website-next/docs
+cp -r $ROOT_DIR/site2/website-next/docs/assets/* $ROOT_DIR/site2/website-next/static/assets
+rm -rf $ROOT_DIR/site2/website-next/docs/assets
+cp -r $SRC_DIR/website/versioned_docs/* $ROOT_DIR/site2/website-next/versioned_docs
+cp -r $SRC_DIR/website/versioned_sidebars/* $ROOT_DIR/site2/website-next/versioned_sidebars
+cp -r $SRC_DIR/website/sidebars.json $ROOT_DIR/site2/website-next/sidebars.json
+cp -r $SRC_DIR/website/versions.json $ROOT_DIR/site2/website-next/versions.json
+cp -r $SRC_DIR/website/releases.json $ROOT_DIR/site2/website-next/releases.json
+cp -r $SRC_DIR/website/pulsar-manager-release-notes.md $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-manager-release-notes.md
+cp -r $SRC_DIR/website/pulsar-manager-release.json $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-manager-release.json
+cp -r $SRC_DIR/website/pulsar-adapters-release.json $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-adapters-release.json
+cp -r $SRC_DIR/website/static/swagger/* $ROOT_DIR/site2/website-next/static/swagger/
+cp -r $SRC_DIR/website/static/api $ROOT_DIR/site2/website-next/static/
 
-cp -r pulsar/site2/website/versions.json pulsar-site/site2/website/versions.json
-cp -r pulsar/site2/website/releases.json pulsar-site/site2/website/releases.json
-cp -r pulsar/site2/website/versions.json pulsar-site/site2/website-next/versions.json
-cp -r pulsar/site2/website/releases.json pulsar-site/site2/website-next/releases.json
-
-cp -r pulsar/site2/website/release-notes.md pulsar-site/site2/website/release-notes.md
-cp -r pulsar/site2/website/pulsar-manager-release-notes.md pulsar-site/site2/website/pulsar-manager-release-notes.md
-cp -r pulsar/site2/website/pulsar-manager-release-notes.md pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release-notes.md
-cp -r pulsar/site2/website/pulsar-manager-release.json pulsar-site/site2/website/pulsar-manager-release.json
-cp -r pulsar/site2/website/pulsar-manager-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release.json
-cp -r pulsar/site2/website/pulsar-adapters-release.json pulsar-site/site2/website/pulsar-adapters-release.json
-cp -r pulsar/site2/website/pulsar-adapters-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-adapters-release.json
-
-cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website/static/swagger/
-cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website-next/static/swagger/
-
-cp -r pulsar/site2/website/static/api pulsar-site/site2/website/static/
-cp -r pulsar/site2/website/static/api pulsar-site/site2/website-next/static/
-
-cd pulsar-site/site2/website-next
+cd $ROOT_DIR/site2/website-next
 if [ -d "node_modules" ]; then
     echo "skip install"
 else
@@ -40,21 +30,3 @@ else
 fi
 
 node scripts/split-swagger-by-version.js
-node migrate/migrate-blogs.js
-node migrate/migrate-full.js
-
-# --------------------------------------- After new website launched and old website backed up ---------------------------------------
-# cp -r pulsar/site2/blog/* pulsar-site/site2/website-next/blog
-# cp -r pulsar/site2/docs/* pulsar-site/site2/website-next/docs
-# cp -r pulsar-site/site2/website-next/docs/assets/* pulsar-site/site2/website-next/static/assets
-# rm -rf pulsar-site/site2/website-next/docs/assets
-# cp -r pulsar/site2/website/versioned_docs/* pulsar-site/site2/website-next/versioned_docs
-# cp -r pulsar/site2/website/versioned_sidebars/* pulsar-site/site2/website-next/versioned_sidebars
-# cp -r pulsar/site2/website/sidebars.json pulsar-site/site2/website-next/sidebars.json
-# cp -r pulsar/site2/website/versions.json pulsar-site/site2/website-next/versions.json
-# cp -r pulsar/site2/website/releases.json pulsar-site/site2/website-next/releases.json
-# cp -r pulsar/site2/website/pulsar-manager-release-notes.md pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release-notes.md
-# cp -r pulsar/site2/website/pulsar-manager-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-manager-release.json
-# cp -r pulsar/site2/website/pulsar-adapters-release.json pulsar-site/site2/website-next/pulsar-manager/pulsar-adapters-release.json
-# cp -r pulsar/site2/website/static/swagger/* pulsar-site/site2/website-next/static/swagger/
-# cp -r pulsar/site2/website/static/api pulsar-site/site2/website-next/static/
