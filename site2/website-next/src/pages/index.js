209ea2fcfba8ad822fa87743c0b96454c3a462f2
Dave Fisher <dave2wave@comcast.net>
2022-07-06 14:00:18 -0700

    Use ® and ™ on first use of Apache and Pulsar

--- site2/website-next/src/pages/index.js
+++ site2/website-next/src/pages/index.js
@@ -165,7 +165,7 @@ export default function Home() {
             <div className="md:float-left md:w-2/3">
               <h1>Cloud-Native, Distributed Messaging and Streaming</h1>
               <p>
-                Apache Pulsar is a distributed, open source pub-sub messaging
+                Apache® Pulsar™ is a distributed, open source pub-sub messaging
                 and streaming platform for real-time workloads, managing
                 hundreds of billions of events per day.
               </p>
