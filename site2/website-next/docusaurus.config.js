9944f9ad1a3ae8c57ccae6e1a3b1638d4b53ea53
Dave Fisher <dave2wave@comcast.net>
2022-07-06 20:45:56 -0700

    Fix Website Trademark Attribution (#139)
    We need to make clear that Pulsar is a trademark of the ASF.

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -338,7 +338,7 @@ module.exports = {
         },
       ],
       copyright: `<p>Apache Pulsar is available under the Apache License, version 2.0.</p>
-      <p>Copyright © ${new Date().getFullYear()} The Apache Software Foundation. All Rights Reserved. Apache, Apache Pulsar and the Apache feather logo are trademarks of The Apache Software Foundation.</p>`,
+      <p>Copyright © ${new Date().getFullYear()} The Apache Software Foundation. All Rights Reserved. Apache, Pulsar, Apache Pulsar, and the Apache feather logo are trademarks or registered trademarks of The Apache Software Foundation.</p>`,
     },
     prism: {
       // theme: lightCodeTheme,
