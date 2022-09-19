1df6d06f60818bde20eaa483e7d507586edf6a8b
Mercurio <signormercurio@gmail.com>
2022-09-19 14:03:13 +0800

    fix: typo in pulsar-config-doc-gen script
    Signed-off-by: Mercurio <signormercurio@gmail.com>

--- site2/tools/pulsar-config-doc-gen.sh
+++ site2/tools/pulsar-config-doc-gen.sh
@@ -33,7 +33,7 @@ GEN_DOCS_PROXY=org.apache.pulsar.proxy.util.CmdGenerateDocumentation
 CLIENT_DIR=$WEBSITE/docsify/client
 CLIENT_CP=$ROOT_DIR/pulsar-client/target/classes
 CLIENT_CONF=org.apache.pulsar.client.impl.conf
-GEN_DOCS_CLIENT=$CLINET_CONF.CmdGenerateDocumentation
+GEN_DOCS_CLIENT=$CLIENT_CONF.CmdGenerateDocumentation
 
 # config doc gen
 cp $WEBSITE/docs/reference-configuration*.md $DOCS_DIR
