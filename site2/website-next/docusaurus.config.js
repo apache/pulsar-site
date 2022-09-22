50058560f46c367216aeca9d66aefe85dd95fda9
tison <wander4096@gmail.com>
2022-09-22 21:04:41 +0800

    change the widge 'Docs' dest to latest stable version
    Signed-off-by: tison <wander4096@gmail.com>

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -195,7 +195,7 @@ module.exports = {
           ],
         },
         {
-          href: "/docs/next",
+          href: "/docs",
           position: "right",
           label: "Docs",
         },
