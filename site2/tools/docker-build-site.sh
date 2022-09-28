68b1a9029f5212872a5230663427ddd2b6ac93b2
Li Li <urfree@apache.org>
2022-09-28 10:14:54 +0800

    add gh cli in container (#232)

--- site2/tools/docker-build-site.sh
+++ site2/tools/docker-build-site.sh
@@ -27,7 +27,7 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $ROOT_DIR/pulsar-client-cpp
 
 BUILD_IMAGE_NAME="${BUILD_IMAGE_NAME:-apachepulsar/pulsar-build}"
-BUILD_IMAGE_VERSION="${BUILD_IMAGE_VERSION:-website-v1}"
+BUILD_IMAGE_VERSION="${BUILD_IMAGE_VERSION:-website-v2}"
 
 IMAGE="$BUILD_IMAGE_NAME:$BUILD_IMAGE_VERSION"
 
