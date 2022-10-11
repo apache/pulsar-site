873832201d14af7206e2d853c1dc2f20fcef2ff7
Mercurio <signormercurio@gmail.com>
2022-10-11 14:41:07 +0800

    fix: restore release-*.js
    Signed-off-by: Mercurio <signormercurio@gmail.com>

---
+++ site2/website-next/data/release-ws.js
@@ -0,0 +1,18 @@
+module.exports = [
+{tagName: "v2.10.1",vtag:"2.10.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.10.1/",doc:"https://pulsar.apache.org/docs//client-libraries-websocket",version:"v2.10.x"},
+{tagName: "v2.10.0",vtag:"2.10.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.10.0/",doc:"https://pulsar.apache.org/docs//client-libraries-websocket",version:""},
+{tagName: "v2.9.3",vtag:"2.9.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.9.3/",doc:"https://pulsar.apache.org/docs/2.9.x/client-libraries-websocket",version:"v2.9.x"},
+{tagName: "v2.9.2",vtag:"2.9.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.9.2/",doc:"https://pulsar.apache.org/docs/2.9.x/client-libraries-websocket",version:""},
+{tagName: "v2.8.4",vtag:"2.8.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.8.4/",doc:"https://pulsar.apache.org/docs/2.8.x/client-libraries-websocket",version:"v2.8.x"},
+{tagName: "v2.8.3",vtag:"2.8.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.8.3/",doc:"https://pulsar.apache.org/docs/2.8.x/client-libraries-websocket",version:""},
+{tagName: "v2.8.2",vtag:"2.8.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.8.2/",doc:"https://pulsar.apache.org/docs/2.8.x/client-libraries-websocket",version:""},
+{tagName: "v2.8.1",vtag:"2.8.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.8.1/",doc:"https://pulsar.apache.org/docs/2.8.x/client-libraries-websocket",version:""},
+{tagName: "v2.8.0",vtag:"2.8.x",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.8.0/",doc:"https://pulsar.apache.org/docs/2.8.x/client-libraries-websocket",version:""},
+{tagName: "v2.7.3",vtag:"2.7.3",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.7.3/",doc:"https://pulsar.apache.org/docs/2.7.3/client-libraries-websocket",version:""},
+{tagName: "v2.7.2",vtag:"2.7.2",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.7.2/",doc:"https://pulsar.apache.org/docs/2.7.2/client-libraries-websocket",version:""},
+{tagName: "v2.7.1",vtag:"2.7.1",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.7.1/",doc:"https://pulsar.apache.org/docs/2.7.1/client-libraries-websocket",version:""},
+{tagName: "v2.6.3",vtag:"2.6.3",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.6.3/",doc:"https://pulsar.apache.org/docs/2.6.3/client-libraries-websocket",version:""},
+{tagName: "v2.6.2",vtag:"2.6.2",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.6.2/",doc:"https://pulsar.apache.org/docs/2.6.2/client-libraries-websocket",version:""},
+{tagName: "v2.6.0",vtag:"2.6.0",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.6.0/",doc:"https://pulsar.apache.org/docs/2.6.0/client-libraries-websocket",version:""},
+{tagName: "v2.5.1",vtag:"2.5.1",releaseNotes:"https://pulsar.apache.org/release-notes/versioned/client-websocket-2.5.1/",doc:"https://pulsar.apache.org/docs/2.5.1/client-libraries-websocket",version:""},
+]
