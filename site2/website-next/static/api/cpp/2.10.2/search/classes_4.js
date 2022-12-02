fb02639e31c94995bc084f5a3e33ba51765c75b1
tison <wander4096@gmail.com>
2022-12-02 14:48:16 +0800

    Remove legacy API docs
    Signed-off-by: tison <wander4096@gmail.com>

--- site2/website-next/static/api/cpp/2.10.2/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['encryptionkeyinfo_0',['EncryptionKeyInfo',['../classpulsar_1_1_encryption_key_info.html',1,'pulsar']]]
-];
--- site2/website-next/static/api/pulsar-functions/2.10.2/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.pulsar.functions.api"},{"l":"org.apache.pulsar.functions.api.state"},{"l":"org.apache.pulsar.functions.api.utils"}];updateSearchResults();
