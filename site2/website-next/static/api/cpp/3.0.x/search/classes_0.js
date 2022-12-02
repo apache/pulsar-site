1ee75e461f9bc84ebaadeabe2ef0820132890c28
tison <wander4096@gmail.com>
2022-12-02 14:36:09 +0800

    Generate C++ API docs for maintained versions
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ site2/website-next/static/api/cpp/3.0.x/search/classes_0.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['authathenz_0',['AuthAthenz',['../classpulsar_1_1_auth_athenz.html',1,'pulsar']]],
+  ['authbasic_1',['AuthBasic',['../classpulsar_1_1_auth_basic.html',1,'pulsar']]],
+  ['authentication_2',['Authentication',['../classpulsar_1_1_authentication.html',1,'pulsar']]],
+  ['authenticationdataprovider_3',['AuthenticationDataProvider',['../classpulsar_1_1_authentication_data_provider.html',1,'pulsar']]],
+  ['authfactory_4',['AuthFactory',['../classpulsar_1_1_auth_factory.html',1,'pulsar']]],
+  ['authoauth2_5',['AuthOauth2',['../classpulsar_1_1_auth_oauth2.html',1,'pulsar']]],
+  ['authtls_6',['AuthTls',['../classpulsar_1_1_auth_tls.html',1,'pulsar']]],
+  ['authtoken_7',['AuthToken',['../classpulsar_1_1_auth_token.html',1,'pulsar']]]
+];
