d796d925ae21b2f88c9ca50b4da880743c678afe
tison <wander4096@gmail.com>
2022-12-03 10:35:27 +0800

    finish site-updater
    Signed-off-by: tison <wander4096@gmail.com>

--- tools/pytools/lib/execute/site_syncer.py
+++ tools/pytools/lib/execute/site_syncer.py
@@ -16,60 +16,36 @@
 # under the License.
 
 import shutil
-import tempfile
 from pathlib import Path
 
-from command import find_command, run, run_pipe
+from constant import site_path
 from execute import swagger_sorter
 
 
-def execute(dry_run: bool):
-    git = find_command('git', msg="git is required")
+def execute(master: Path):
+    main = master / 'site2'
+    site = site_path()
 
-    with tempfile.TemporaryDirectory() as cwd:
-        print(f"cwd={cwd} dry_run={dry_run}")
-        run(git, 'clone', '-b', 'main', '--depth', '1', 'https://github.com/apache/pulsar-site', cwd=cwd)
-        run(git, 'clone', '-b', 'master', '--depth', '1', 'https://github.com/apache/pulsar', cwd=cwd)
-        main = Path(cwd) / 'pulsar' / 'site2'
-        site = Path(cwd) / 'pulsar-site' / 'site2' / 'website-next'
-        shutil.copytree(main / 'docs', site / 'docs', dirs_exist_ok=True)
-        shutil.copytree(site / 'docs' / 'assets', site / 'static' / 'assets', dirs_exist_ok=True)
-        shutil.rmtree(site / 'docs' / 'assets')
+    shutil.copytree(main / 'docs', site / 'docs', dirs_exist_ok=True)
+    shutil.copytree(site / 'docs' / 'assets', site / 'static' / 'assets', dirs_exist_ok=True)
+    shutil.rmtree(site / 'docs' / 'assets')
 
-        main = main / 'website'
-        shutil.copytree(main / 'versioned_docs', site / 'versioned_docs', dirs_exist_ok=True)
-        shutil.copytree(main / 'versioned_sidebars', site / 'versioned_sidebars', dirs_exist_ok=True)
-        shutil.move(main / 'sidebars.json', site / 'sidebars.json')
-        shutil.move(main / 'versions.json', site / 'versions.json')
-        shutil.move(main / 'releases.json', site / 'releases.json')
+    main = main / 'website'
+    shutil.copytree(main / 'versioned_docs', site / 'versioned_docs', dirs_exist_ok=True)
+    shutil.copytree(main / 'versioned_sidebars', site / 'versioned_sidebars', dirs_exist_ok=True)
+    shutil.copy2(main / 'sidebars.json', site / 'sidebars.json')
+    shutil.copy2(main / 'versions.json', site / 'versions.json')
+    shutil.copy2(main / 'releases.json', site / 'releases.json')
 
-        shutil.move(
-            main / 'pulsar-manager-release-notes.md',
-            site / 'pulsar-manager' / 'pulsar-manager-release-notes.md')
-        shutil.move(
-            main / 'pulsar-manager-release.json',
-            site / 'pulsar-manager' / 'pulsar-manager-release.json')
-        shutil.move(
-            main / 'pulsar-adapters-release.json',
-            site / 'pulsar-manager' / 'pulsar-adapters-release.json')
+    shutil.copy2(
+        main / 'pulsar-manager-release-notes.md',
+        site / 'pulsar-manager' / 'pulsar-manager-release-notes.md')
+    shutil.copy2(
+        main / 'pulsar-manager-release.json',
+        site / 'pulsar-manager' / 'pulsar-manager-release.json')
+    shutil.copy2(
+        main / 'pulsar-adapters-release.json',
+        site / 'pulsar-manager' / 'pulsar-adapters-release.json')
 
-        shutil.copytree(main / 'static' / 'swagger', site / 'static' / 'swagger', dirs_exist_ok=True)
-        swagger_sorter.execute(site / 'static' / 'swagger')
-
-        main = Path(cwd) / 'pulsar'
-        commit = run_pipe(git, 'rev-parse', '--short', 'HEAD', cwd=main).read().strip()
-
-        site = Path(cwd) / 'pulsar-site'
-        run(git, 'add', '-A', '.', cwd=site)
-        run(git, 'status', cwd=site)
-        run(git, 'config', 'user.name', 'Pulsar Site Updater', cwd=site)
-        run(git, 'config', 'user.email', 'dev@pulsar.apache.org', cwd=site)
-        run(git, 'remote', '-v', cwd=site)
-        run(git, 'commit', '--allow-empty', '-m', f'Docs sync done from apache/pulsar (#{commit})', cwd=site)
-
-        if not dry_run:
-            run(git, 'push', 'origin', 'main', cwd=site)
-
-
-if __name__ == '__main__':
-    execute(True)
+    shutil.copytree(main / 'static' / 'swagger', site / 'static' / 'swagger', dirs_exist_ok=True)
+    swagger_sorter.execute(site / 'static' / 'swagger')
