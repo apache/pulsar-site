e3d78e5124a5167a7203d8945a0abdec816c130a
tison <wander4096@gmail.com>
2022-12-13 15:22:05 +0800

    cleanup swagger scripts
    Signed-off-by: tison <wander4096@gmail.com>

--- site2/website-next/scripts/sync-docs.sh
+++ site2/website-next/scripts/sync-docs.sh
@@ -18,7 +18,6 @@ cp -r $SRC_DIR/website/releases.json $ROOT_DIR/site2/website-next/releases.json
 cp -r $SRC_DIR/website/pulsar-manager-release-notes.md $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-manager-release-notes.md
 cp -r $SRC_DIR/website/pulsar-manager-release.json $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-manager-release.json
 cp -r $SRC_DIR/website/pulsar-adapters-release.json $ROOT_DIR/site2/website-next/pulsar-manager/pulsar-adapters-release.json
-cp -r $SRC_DIR/website/static/swagger/* $ROOT_DIR/site2/website-next/static/swagger/
 cp -r $SRC_DIR/website/static/api $ROOT_DIR/site2/website-next/static/
 
 cd $ROOT_DIR/site2/website-next
@@ -27,7 +26,5 @@ if [ -d "node_modules" ]; then
 else
     yarn install
 fi
-
-node scripts/split-swagger-by-version.js
 
 echo "full sync done..."
