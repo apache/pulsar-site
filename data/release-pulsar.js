fd3b67f78bff85c8607789470be658724f37259d
momo-jun <jma@streamnative.io>
2023-01-09 16:10:08 +0800

    Update the Release page to link the new blog

--- data/release-pulsar.js
+++ data/release-pulsar.js
@@ -5,7 +5,7 @@ module.exports = [
         "publishedAt": "2023-01-04T01:53:34Z",
         "vtag": "2.10.x",
         "releaseNotes": "/release-notes/versioned/pulsar-2.10.3/",
-        "releaseBlog": "",
+        "releaseBlog": "/blog/2023/01/09/Apache-Pulsar-2-10-3",
         "doc": "/docs/2.10.x",
         "version": "v2.10.x"
     },
