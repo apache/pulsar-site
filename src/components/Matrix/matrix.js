8d14d052c9f9c49f1496c9fdbeaaa05971809343
visortelle <visortelle@gmail.com>
2023-05-15 12:25:16 +0200

    Client feature matrix - Fix column overlap with content when scroll table horizontally

--- src/components/Matrix/matrix.js
+++ src/components/Matrix/matrix.js
@@ -2,29 +2,26 @@ import React from "react";
 import "react-base-table/styles.css";
 import BaseTable, { AutoResizer } from "react-base-table";
 import { fixedColumns, getStyle, genData, rowRenderer } from "./util.js";
+import s from "./matrix.module.css";
 
 export default function Matrix(props) {
   const data = props.data;
   const name = props.name;
 
   return (
-    <div>
-      <div key={name}>
-        <div style={getStyle(name)}>
-          <AutoResizer>
-            {({ width, height }) => (
-              <BaseTable
-                fixed
-                width={width}
-                height={height}
-                columns={fixedColumns}
-                data={genData(data)}
-                rowRenderer={rowRenderer}
-              />
-            )}
-          </AutoResizer>
-        </div>
-      </div>
+    <div className={s.Matrix} style={getStyle(name)}>
+      <AutoResizer>
+        {({ width, height }) => (
+          <BaseTable
+            fixed
+            width={width}
+            height={height}
+            columns={fixedColumns}
+            data={genData(data)}
+            rowRenderer={rowRenderer}
+          />
+        )}
+      </AutoResizer>
     </div>
   );
 }
