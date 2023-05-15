ea0464f31f9d416670a0e549a5396c81a76fd635
visortelle <visortelle@gmail.com>
2023-05-15 11:53:53 +0200

    Fix client-feature-matrix tables height

--- src/components/Matrix/matrix.js
+++ src/components/Matrix/matrix.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "react-base-table/styles.css";
 import BaseTable, { AutoResizer } from "react-base-table";
-import { fixedColumns, getHeight, genData, rowRenderer } from "./util.js";
+import { fixedColumns, getStyle, genData, rowRenderer } from "./util.js";
 
 export default function Matrix(props) {
   const data = props.data;
   const name = props.name;
+
   return (
-    <div className="tailwind">
-      <div className="mb-8" key={name}>
-        <div className={`w-full ${getHeight(name)}`}>
+    <div>
+      <div key={name}>
+        <div style={getStyle(name)}>
           <AutoResizer>
             {({ width, height }) => (
               <BaseTable
