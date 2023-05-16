ed5d19862c7ea7d45813b78b4bba74cb287ad241
Yunze Xu <xyzinfernity@163.com>
2023-05-16 20:40:08 +0800

    Generate C++ client 3.2.0 doc (#577)

---
+++ static/api/cpp/3.2.x/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['batchreceivepolicy_486',['BatchReceivePolicy',['../classpulsar_1_1_batch_receive_policy.html',1,'pulsar']]],
+  ['brokerconsumerstats_487',['BrokerConsumerStats',['../classpulsar_1_1_broker_consumer_stats.html',1,'pulsar']]]
+];
