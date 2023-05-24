b5f6573cbfd868da5347259e054db74dd9006dd1
visortelle <visortelle@gmail.com>
2023-05-24 09:13:59 +0200

    Pulsar Virtual Summit Europe 2023 is over. Remove announcement.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,16 +142,16 @@ module.exports = {
   trailingSlash: true,
   themeConfig: {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    announcementBar: {
-      id: "summit",
-      content: renderAnnouncementBar(
-        "Pulsar Virtual Summit Europe 2023",
-        "https://pulsar-summit.org/event/europe-2023"
-      ),
-      backgroundColor: "#282826",
-      textColor: "#fff",
-      isCloseable: false,
-    },
+    // announcementBar: {
+    //   id: "summit",
+    //   content: renderAnnouncementBar(
+    //     "Pulsar Virtual Summit Europe 2023",
+    //     "https://pulsar-summit.org/event/europe-2023"
+    //   ),
+    //   backgroundColor: "#282826",
+    //   textColor: "#fff",
+    //   isCloseable: false,
+    // },
     colorMode: {
       disableSwitch: true,
     },
