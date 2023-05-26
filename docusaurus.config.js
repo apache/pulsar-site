1fab8660693503b7e845b4e4c454336ea6b4b351
visortelle <visortelle@gmail.com>
2023-05-26 09:14:16 +0200

    Fix highlight Quickstart and Concepts menu items at the same time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 const linkifyRegex = require("./plugins/remark-linkify-regex");
 const { renderAnnouncementBar } = require("./src/components/ui/renderAnnouncementBar");
 const versions = require("./versions.json");
+const latestStableVersion = versions[0];
 const versionsMap = {
     ..._.keyBy(
         versions.map((item) => {
@@ -172,14 +173,14 @@ module.exports = {
           label: "Get Started",
           position: "left",
           items: [
-            {
-              type: 'doc',
-              docId: 'concepts-overview',
+               {
+              to: `/docs/${latestStableVersion}/concepts-overview/`,
+              activeBaseRegex: `docs/(${versions.join('|')})/concepts-overview/$`,
               label: "Concepts",
             },
             {
-              type: 'doc',
-              docId: 'about',
+              to: `/docs/${latestStableVersion}/`,
+              activeBaseRegex: `docs/(${versions.join('|')})/$`,
               label: "Quickstart",
             },
             {
