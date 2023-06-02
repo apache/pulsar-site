6052f0d3fd581373a764bd61fcf6d9ee3e9a3087
labuladong <labuladong@foxmail.com>
2023-06-02 11:46:32 +0800

    [improve] homepage button redirects to latest stable version (#597)
    Co-authored-by: tison <wander4096@gmail.com>

--- src/components/pages/HomePage/ShortInfo/ShortInfo.tsx
+++ src/components/pages/HomePage/ShortInfo/ShortInfo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import Button from '../../../ui/Button/Button';
+import Button from '@site/src/components/ui/Button/Button';
 import Parallax from './Parallax/Parallax';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import s from './ShortInfo.module.css';
 import ScreenTitle from '../ui/ScreenTitle/ScreenTitle';
+
+const versions = require("@site/versions.json");
+const latestVersion = versions[0];
 
 const ShortInfo: React.FC = () => {
   const { siteConfig } = useDocusaurusContext();
@@ -25,12 +28,12 @@ const ShortInfo: React.FC = () => {
               <Button
                 title='Explore docs'
                 variant='action'
-                href={`${siteConfig.baseUrl}docs/next`}
+                href={`${siteConfig.baseUrl}docs/${latestVersion}`}
               />
               <Button
                 title='Quickstart'
                 variant='regular'
-                href={`${siteConfig.baseUrl}docs/next/concepts-overview`}
+                href={`${siteConfig.baseUrl}docs/${latestVersion}/concepts-overview`}
               />
             </div>
           </div>
