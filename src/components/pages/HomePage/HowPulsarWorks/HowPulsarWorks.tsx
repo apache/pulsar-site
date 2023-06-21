467ef55b4e7f215f941415e5c67da7408dc4e17c
Kiryl Valkovich <visortelle@gmail.com>
2023-06-21 10:56:59 +0300

    Update src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
    Co-authored-by: Anonymitaet <50226895+Anonymitaet@users.noreply.github.com>

--- src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
+++ src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
@@ -41,7 +41,7 @@ const cards: CardProps[] = [
     children: (
       <p>
         Topics (i.e., partitions) are divided among Pulsar brokers.
-        A broker receives messages for a topic and appends them to the topic’s active virtual file (a.k.a Ledger),
+        A broker receives messages for a topic and appends them to the topic’s active virtual file (a.k.a ledger),
         hosted on the Bookkeeper cluster. Brokers read messages from the cache (mostly) or Bookkeeper and dispatch them to the Consumers.
         Brokers also receive message acknowledgments and persist them to the Bookkeeper as well.
         They are stateless (don’t use/need a disk).
