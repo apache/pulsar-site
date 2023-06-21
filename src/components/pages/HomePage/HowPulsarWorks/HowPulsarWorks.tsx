0c69a8a68f298a1522830e19842d1fe7b2d84f52
Kiryl Valkovich <visortelle@gmail.com>
2023-06-21 10:58:12 +0300

    Update src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
    Co-authored-by: Anonymitaet <50226895+Anonymitaet@users.noreply.github.com>

--- src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
+++ src/components/pages/HomePage/HowPulsarWorks/HowPulsarWorks.tsx
@@ -56,7 +56,7 @@ const cards: CardProps[] = [
         Apache BookKeeper is a cluster of nodes called bookies.
         Each virtual file (a.k.a ledger) is divided into consecutive segments, and each segment is kept on 3 Bookies by default
         (replicated by the client - i.e., the broker).
-        Operators can add Bookies rapidly since no data reshuffling (moving) between them is required.
+        Operators can add bookies rapidly since no data reshuffling (moving) between them is required.
         They immediately share the incoming write load.
       </p>
     )
