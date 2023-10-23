ca5e2cc742cd862c99f984755e3802d1913e2511
tison <wander4096@gmail.com>
2023-10-23 15:33:09 +0800

    Add more features
    Signed-off-by: tison <wander4096@gmail.com>

--- data/broker-features.js
+++ data/broker-features.js
@@ -1,6 +1,26 @@
 export default [
     {
         feature: "Produce / Exclusive",
+        stableSince: "2.2.0",
+        document: [
+            {
+                text: "Concept",
+                link: "concepts-clients/#access-mode",
+            },
+        ],
+    },
+    {
+        feature: "Produce / Shared",
+        stableSince: "2.2.0",
+        document: [
+            {
+                text: "Concept",
+                link: "concepts-clients/#access-mode",
+            },
+        ],
+    },
+    {
+        feature: "Subscription / Exclusive",
         experimentalSince: "",
         betaSince: "",
         stableSince: "2.2.0",
