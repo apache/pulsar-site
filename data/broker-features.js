4e412881c4bb079c17efa4cea23f5279ea0daeb0
tison <wander4096@gmail.com>
2023-10-30 16:05:51 +0800

    add more features
    Signed-off-by: tison <wander4096@gmail.com>

--- data/broker-features.js
+++ data/broker-features.js
@@ -4,8 +4,8 @@ export default [
         stableSince: "2.2.0",
         document: [
             {
-                text: "Concept",
-                link: "concepts-clients/#access-mode",
+                "text": "Concept",
+                "link": "concepts-clients/#access-mode",
             },
         ],
     },
@@ -14,8 +14,8 @@ export default [
         stableSince: "2.2.0",
         document: [
             {
-                text: "Concept",
-                link: "concepts-clients/#access-mode",
+                "text": "Concept",
+                "link": "concepts-clients/#access-mode",
             },
         ],
     },
@@ -26,8 +26,8 @@ export default [
         stableSince: "2.2.0",
         document: [
             {
-                text: "Concept",
-                link: "concepts-messaging/#exclusive",
+                "text": "Concept",
+                "link": "concepts-messaging/#exclusive",
             },
         ],
     },
@@ -38,8 +38,8 @@ export default [
         stableSince: "2.2.0",
         document: [
             {
-                text: "Concept",
-                link: "concepts-messaging/#failover",
+                "text": "Concept",
+                "link": "concepts-messaging/#failover",
             },
         ],
     },
@@ -50,8 +50,8 @@ export default [
         stableSince: "2.2.0",
         document: [
             {
-                text: "Concept",
-                link: "concepts-messaging/#shared",
+                "text": "Concept",
+                "link": "concepts-messaging/#shared",
             },
         ],
     },
@@ -62,9 +62,194 @@ export default [
         stableSince: "",
         document: [
             {
-                text: "Concept",
-                link: "concepts-messaging/#key_shared",
+                "text": "Concept",
+                "link": "concepts-messaging/#key_shared",
             },
+        ],
+    },
+    {
+        feature: "Messaging / Retry Letter Topic",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-messaging/#retry-letter-topic",
+            }
+        ],
+    },
+    {
+        feature: "Messaging / Dead Letter Topic",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-messaging/#dead-letter-topic",
+            }
+        ],
+    },
+    {
+        feature: "Messaging / Delay Message Delivery",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-messaging/#delayed-message-delivery",
+            }
+        ],
+    },
+    {
+        feature: "Messaging / Batching",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-messaging/#batching",
+            }
+        ],
+    },
+    {
+        feature: "Messaging / Chunking",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-messaging/#chunking",
+            }
+        ],
+    },
+    {
+        feature: "Transaction / Failover Subscription",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [],
+    },
+    {
+        feature: "Transaction / Shared Subscription",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [],
+    },
+    {
+        feature: "Transaction / Segmented Snapshot",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [],
+    },
+    {
+        feature: "Schema / KeyValue",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Quickstart",
+                "link": "schema-get-started/#keyvalue"
+            }
+        ],
+    },
+    {
+        feature: "Schema / Avro",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Quickstart",
+                "link": "schema-get-started/#avro"
+            }
+        ],
+    },
+    {
+        feature: "Schema / JSON",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Quickstart",
+                "link": "schema-get-started/#json"
+            }
+        ],
+    },
+    {
+        feature: "Functions / Java",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "functions-concepts",
+            },
+            {
+                "text": "Quickstart",
+                "link": "functions-quickstart",
+            }
+        ],
+    },
+    {
+        feature: "Functions / Golang",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "functions-concepts",
+            }
+        ],
+    },
+    {
+        feature: "Functions / Python",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "functions-concepts",
+            }
+        ],
+    },
+    {
+        feature: "Multitenancy",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-multi-tenancy",
+            }
+        ],
+    },
+    {
+        feature: "Geo Replication",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                "text": "Concept",
+                "link": "concepts-replication",
+            },
+            {
+                "text": "Operation",
+                "link": "administration-geo",
+            }
         ],
     },
 ]
