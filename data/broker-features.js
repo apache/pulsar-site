f51a6afd8b4dbd766c8957fa8bacb87e00baea70
tison <wander4096@gmail.com>
2023-10-30 15:50:40 +0800

    basic pubsub features
    Signed-off-by: tison <wander4096@gmail.com>

--- data/broker-features.js
+++ data/broker-features.js
@@ -27,7 +27,43 @@ export default [
         document: [
             {
                 text: "Concept",
-                link: "concepts-clients/#access-mode",
+                link: "concepts-messaging/#exclusive",
+            },
+        ],
+    },
+    {
+        feature: "Subscription / Failover",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "2.2.0",
+        document: [
+            {
+                text: "Concept",
+                link: "concepts-messaging/#failover",
+            },
+        ],
+    },
+    {
+        feature: "Subscription / Shared",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "2.2.0",
+        document: [
+            {
+                text: "Concept",
+                link: "concepts-messaging/#shared",
+            },
+        ],
+    },
+    {
+        feature: "Subscription / KeyShared",
+        experimentalSince: "",
+        betaSince: "",
+        stableSince: "",
+        document: [
+            {
+                text: "Concept",
+                link: "concepts-messaging/#key_shared",
             },
         ],
     },
