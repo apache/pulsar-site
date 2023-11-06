89ac320a0cf6716b91d5984c188dea219c75b4fc
tison <wander4096@gmail.com>
2023-11-06 18:03:40 +0800

    misc: remove banner after the pulsar summit (#724)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/components/pages/EventsPage/FeaturedEvent/FeaturedEvent.tsx
+++ src/components/pages/EventsPage/FeaturedEvent/FeaturedEvent.tsx
@@ -9,39 +9,39 @@ const FeaturedEvent: React.FC = () => {
     return null;
   }
 
-  return (
-    <div className={s.FeaturedEvent}>
-      <div>Featured event</div>
-      <h2>Pulsar Summit North America 2023</h2>
-      <p>
-        Mark your calendars! Pulsar Summit North America is coming back to Hotel Nikko in San Francisco, California on Wednesday, October 25th, 2023.
-      </p>
-      <p>
-        Over the past few years, Pulsar Summit has featured 100+ speakers and brought together more than 2,0000 Pulsar community members for engaging and insightful sessions.
-      </p>
-      <p>
-        <a href="https://sessionize.com/pulsar-summit-north-america-2023/" target='_blank'><strong>CFPs are now open!</strong></a>
-      </p>
-    </div>
-  );
-
-  // Uncomment the section above if no featured event is available.
   // return (
   //   <div className={s.FeaturedEvent}>
-  //     {/* <div>Featured event</div> */}
-  //     <h2>Pulsar Summit</h2>
+  //     <div>Featured event</div>
+  //     <h2>Pulsar Summit North America 2023</h2>
   //     <p>
-  //       Pulsar Summit is the conference for Apache Pulsar and event processing communities that takes place multiple times a year.
+  //       Mark your calendars! Pulsar Summit North America is coming back to Hotel Nikko in San Francisco, California on Wednesday, October 25th, 2023.
   //     </p>
   //     <p>
-  //       This summit provides a diverse platform to explore, learn and exchange ideas. From captivating keynotes to insightful panel discussions, hands-on workshops, and unparalleled networking opportunities.
+  //       Over the past few years, Pulsar Summit has featured 100+ speakers and brought together more than 2,0000 Pulsar community members for engaging and insightful sessions.
   //     </p>
-  //     <a
-  //       href="https://pulsar-summit.org/"
-  //       target='_blank'
-  //     >Learn more</a>
+  //     <p>
+  //       <a href="https://sessionize.com/pulsar-summit-north-america-2023/" target='_blank'><strong>CFPs are now open!</strong></a>
+  //     </p>
   //   </div>
   // );
+
+  // Uncomment the section above if no featured event is available.
+  return (
+    <div className={s.FeaturedEvent}>
+      {/* <div>Featured event</div> */}
+      <h2>Pulsar Summit</h2>
+      <p>
+        Pulsar Summit is the conference for Apache Pulsar and event processing communities that takes place multiple times a year.
+      </p>
+      <p>
+        This summit provides a diverse platform to explore, learn and exchange ideas. From captivating keynotes to insightful panel discussions, hands-on workshops, and unparalleled networking opportunities.
+      </p>
+      <a
+        href="https://pulsar-summit.org/"
+        target='_blank'
+      >Learn more</a>
+    </div>
+  );
 }
 
 export default FeaturedEvent;
