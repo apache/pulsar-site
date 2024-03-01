7fa1ec966ffb9c125159a5f2e224dcefe29c5060
Kiryl Valkovich <visortelle@gmail.com>
2024-03-01 10:15:36 +0400

    Add the "I don't understand it" link (#807)
    Goals:
    - Allow users to quickly ask a question in the right place.
    - Improve the documentation by encouraging users to report unclear or incomplete parts of the documentation.

--- src/components/ui/ArticleThumb/ArticleThumb.js
+++ src/components/ui/ArticleThumb/ArticleThumb.js
@@ -1,56 +1,63 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ThumbDown from './thumbs-down.svg';
 import ThumbUp from './thumbs-up.svg';
+import s from './ArticleThumb.module.css';
 
 const ArticleThumb = () => {
-    const [activeKey, setActiveKey] = useState();
-    const handleThumbClick = (val) => {
-        if (!activeKey) {
-            setActiveKey(val)
-            if (window && (window._paq)) {
-                window._paq.push(['trackEvent', 'Website Feedback', val, location.pathname])
-            }
-        }
-    }
-
-    let card_style = {borderWidth: '1px', padding: '3px', display: 'flex'}
-    let thumb_style = {
-        justifyContent: 'center', display: 'flex', borderRadius: '99999px',
-        width: '2.5rem', height: '2.5rem',
-    }
+  const [activeKey, setActiveKey] = useState();
+  const handleThumbClick = (val) => {
     if (!activeKey) {
-        thumb_style['cursor'] = "pointer"
+      setActiveKey(val)
+      if (window && (window._paq)) {
+        window._paq.push(['trackEvent', 'Website Feedback', val, location.pathname])
+      }
     }
-    return (
-      <div className="border">
-        <div style={{ color: "var(--ifm-toc-link-color)" }}>
-          Was this helpful?
+  }
+
+  let card_style = { borderWidth: '1px', padding: '3px', display: 'flex' }
+  let thumb_style = {
+    justifyContent: 'center', display: 'flex', borderRadius: '99999px',
+    width: '2.5rem', height: '2.5rem',
+  }
+  if (!activeKey) {
+    thumb_style['cursor'] = "pointer"
+  }
+  return (
+    <div className="border">
+      <div style={{ color: "var(--ifm-toc-link-color)" }}>
+        Was this helpful?
+      </div>
+      <div style={card_style}>
+        <div
+          style={{
+            ...thumb_style,
+            background: activeKey === "up" ? "var(--text-color)" : "",
+            color: activeKey === "up" ? "#fff" : "",
+          }}
+          onClick={() => handleThumbClick("up")}
+        >
+          <ThumbUp />
         </div>
-        <div style={card_style}>
-          <div
-            style={{
-              ...thumb_style,
-              background: activeKey === "up" ? "var(--text-color)" : "",
-              color: activeKey === "up" ? "#fff" : "",
-            }}
-            onClick={() => handleThumbClick("up")}
-          >
-            <ThumbUp />
-          </div>
-          <div
-            style={{
-              ...thumb_style,
-              marginLeft: "30px",
-              background: activeKey === "down" ? "var(--text-color)" : "",
-              color: activeKey === "down" ? "#fff" : "",
-            }}
-            onClick={() => handleThumbClick("down")}
-          >
-            <ThumbDown />
-          </div>
+        <div
+          style={{
+            ...thumb_style,
+            background: activeKey === "down" ? "var(--text-color)" : "",
+            color: activeKey === "down" ? "#fff" : "",
+          }}
+          onClick={() => handleThumbClick("down")}
+        >
+          <ThumbDown />
         </div>
       </div>
-    );
+      <a
+        target='_blank'
+        className={s.RequestHelpButton}
+        href="https://github.com/apache/pulsar/discussions/new?category=q-a"
+      >
+        🛟 I don't understand it
+      </a>
+    </div>
+  );
 };
 
 
