49591218428adf2ccb941fbce08cab641767aee5
Kiryl Valkovich 🛸 <visortelle@gmail.com>
2024-03-14 12:03:18 +0400

    Summit EU 2024 announcement bar (#842)
    * Summit EU 2024 announcement bar
    
    * Update docusaurus.config.js
    
    Co-authored-by: Lari Hotari <lhotari@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lari Hotari <lhotari@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,8 +151,8 @@ module.exports = {
     announcementBar: {
       id: "summit",
       content: renderAnnouncementBar(
-        "Submit your stories to Pulsar Summit EMEA! 📣",
-        "https://sessionize.com/pulsar-virtual-summit-europe-2024"
+        "Get your free pass for Pulsar Virtual Summit Europe 2024 on May 14, 2024 🗓️",
+        "https://registration.socio.events/e/pulsarvirtualsummiteurope2024"
       ),
       backgroundColor: "#282826",
       textColor: "#fff",
