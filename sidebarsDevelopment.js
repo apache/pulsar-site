21209b0d1369206170137ab5a33c16047a14c5c5
Lari Hotari <lhotari@users.noreply.github.com>
2024-10-08 13:20:14 +0300

    Add "maintenance tasks & cherry-picking" guide

--- sidebarsDevelopment.js
+++ sidebarsDevelopment.js
@@ -61,6 +61,7 @@ const sidebars = {
                         'release-note-guide',
                     ]
                 },
+                'maintenance-process',
                 'validate-release-candidate',
             ]
         },
