e853a3013e7fcd0a9e6607358d98e0147d33d93a
Baodi Shi <baodi@apache.org>
2025-05-14 15:20:28 +0800

    Addressed comments

--- data/release-python.js
+++ data/release-python.js
@@ -1,5 +1,5 @@
 module.exports = [
-{tagName: "v3.7.0",vtag:"3.6.x",releaseNotes:"/release-notes/versioned/client-python-3.7.0/",doc:"/docs/client-libraries-python",version:"v3.7.x"},
+{tagName: "v3.7.0",vtag:"3.7.x",releaseNotes:"/release-notes/versioned/client-python-3.7.0/",doc:"/docs/client-libraries-python",version:"v3.7.x"},
 {tagName: "v3.6.1",vtag:"3.6.x",releaseNotes:"/release-notes/versioned/client-python-3.6.1/",doc:"/docs/client-libraries-python",version:"v3.6.x"},
 {tagName: "v3.6.0",vtag:"3.6.x",releaseNotes:"/release-notes/versioned/client-python-3.6.0/",doc:"/docs/client-libraries-python",version:""},
 {tagName: "v3.5.0",vtag:"3.5.x",releaseNotes:"/release-notes/versioned/client-python-3.5.0/",doc:"/docs/client-libraries-python",version:"v3.5.x"},
