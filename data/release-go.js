71fd77c88882ce172d3f6e032bdb9afe636316b1
Zike Yang <zike@apache.org>
2025-10-17 17:45:15 +0800

    Add go client release notes for 0.17.0

--- data/release-go.js
+++ data/release-go.js
@@ -1,4 +1,5 @@
 module.exports = [
+{tagName: "v0.17.0",releaseNotes:"/release-notes/versioned/pulsar-client-go-0.17.0/",doc:"/docs/client-libraries-go",version:"v0.17.x"},
 {tagName: "v0.16.0",releaseNotes:"/release-notes/versioned/pulsar-client-go-0.16.0/",doc:"/docs/client-libraries-go",version:"v0.16.x"},
 {tagName: "v0.15.1",releaseNotes:"/release-notes/versioned/pulsar-client-go-0.15.1/",doc:"/docs/client-libraries-go",version:"v0.15.x"},
 {tagName: "v0.15.0",releaseNotes:"/release-notes/versioned/pulsar-client-go-0.15.0/",doc:"/docs/client-libraries-go",version:""},
