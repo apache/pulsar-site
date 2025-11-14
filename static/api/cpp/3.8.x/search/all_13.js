3fa685f34cfe1ebce329baa12ef2d77e144b516b
Yunze Xu <xyzinfernity@163.com>
2025-11-14 11:26:16 +0800

    Add CPP client v3.8.0 API docs

---
+++ static/api/cpp/3.8.x/search/all_13.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['unsubscribe_0',['unsubscribe',['../classpulsar_1_1_consumer.html#a542ca0a9473a03ccf8bd8aeed24de490',1,'pulsar::Consumer']]],
+  ['unsubscribeasync_1',['unsubscribeAsync',['../classpulsar_1_1_consumer.html#a2a3a5c0ca53537d6c2aeecbc9d850e6a',1,'pulsar::Consumer']]]
+];
---
+++ static/api/cpp/3.8.x/search/enumvalues_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['persistentonly_0',['PersistentOnly',['../namespacepulsar.html#abd9b21e56a6fb78e04cae2664ff0dbbda68931062ae96d4dcc4778eeac5fa3de4',1,'pulsar']]],
+  ['protobuf_1',['PROTOBUF',['../namespacepulsar.html#a110c1992e36a25acc192ea4f399bc4f8a5054f171f6c60b73043727ddc60698a0',1,'pulsar']]],
+  ['protobuf_5fnative_2',['PROTOBUF_NATIVE',['../namespacepulsar.html#a110c1992e36a25acc192ea4f399bc4f8a24048f6ae2e1048989e69644edcdc8f6',1,'pulsar']]]
+];
