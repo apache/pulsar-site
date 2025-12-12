4711f64194d520c7c099996f73db4a68b574e35f
Yunze Xu <xyzinfernity@163.com>
2025-12-12 17:11:39 +0800

    Add CPP client v4.0.0 API docs (#1064)

---
+++ static/api/cpp/4.0.x/search/enumvalues_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['defaultbatching_0',['DefaultBatching',['../classpulsar_1_1_producer_configuration.html#a46e36919c57f78feed59e12e84a8b346af2acbf3fc9b0dc49d8af65a148d22e27',1,'pulsar::ProducerConfiguration']]],
+  ['double_1',['DOUBLE',['../namespacepulsar.html#a110c1992e36a25acc192ea4f399bc4f8a1b1684230ae9c15941cb3fe05bf972f0',1,'pulsar']]]
+];
---
+++ static/api/cpp/4.0.x/search/functions_5.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['fileloggerfactory_0',['FileLoggerFactory',['../classpulsar_1_1_file_logger_factory.html#a295a3b87a6a73a67bb8f805b3a4769b5',1,'pulsar::FileLoggerFactory']]],
+  ['flush_1',['flush',['../classpulsar_1_1_producer.html#a1cd59ffc4a23162eca39183ba4278146',1,'pulsar::Producer']]],
+  ['flushasync_2',['flushAsync',['../classpulsar_1_1_producer.html#a78299ce1bcfa317c8f809707bbce138c',1,'pulsar::Producer']]],
+  ['foreach_3',['forEach',['../classpulsar_1_1_table_view.html#a98b532f6704e54f6cb9f40dc9a9ea7d3',1,'pulsar::TableView']]],
+  ['foreachandlisten_4',['forEachAndListen',['../classpulsar_1_1_table_view.html#a1e2179e83cbd5257e424b0808d75da3f',1,'pulsar::TableView']]],
+  ['from_5',['from',['../classpulsar_1_1_message_id_builder.html#adf978433aad184f7bcb11bfbf86f91ee',1,'pulsar::MessageIdBuilder::from(const MessageId &amp;messageId)'],['../classpulsar_1_1_message_id_builder.html#a46bff6912be430a70ff6d236fdef5d4c',1,'pulsar::MessageIdBuilder::from(const proto::MessageIdData &amp;messageIdData)']]]
+];
