1e275c30ebce691c4464e870f5bd318bdc123e35
Yunze Xu <xyzinfernity@163.com>
2026-03-30 16:32:51 +0800

    Add CPP client v4.1.0 API docs (#1100)

---
+++ static/api/cpp/4.1.x/search/classes_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['batchreceivepolicy_0',['BatchReceivePolicy',['../classpulsar_1_1_batch_receive_policy.html',1,'pulsar']]],
+  ['brokerconsumerstats_1',['BrokerConsumerStats',['../classpulsar_1_1_broker_consumer_stats.html',1,'pulsar']]],
+  ['builder_2',['Builder',['../classpulsar_1_1_auto_cluster_failover_1_1_builder.html',1,'pulsar::AutoClusterFailover']]]
+];
---
+++ static/api/cpp/4.1.x/search/enums_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['schematype_0',['SchemaType',['../namespacepulsar.html#a110c1992e36a25acc192ea4f399bc4f8',1,'pulsar']]]
+];
