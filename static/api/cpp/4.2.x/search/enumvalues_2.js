159e6fbc6a131bed1e939e8cf3e60699549fd504
Yunze Xu <xyzinfernity@163.com>
2026-06-15 10:18:35 +0800

    Add CPP client v4.2.0 API docs (#1164)

---
+++ static/api/cpp/4.2.x/search/enumvalues_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['consumerexclusive_0',['ConsumerExclusive',['../namespacepulsar.html#a9001dd59f719a974dc7b4ef5858dda8ea915cd237340dcd1d212f8d398f3d91ac',1,'pulsar']]],
+  ['consumerfailover_1',['ConsumerFailover',['../namespacepulsar.html#a9001dd59f719a974dc7b4ef5858dda8eaadffd5f0d50b1da36685230cd3f910a1',1,'pulsar']]],
+  ['consumerkeyshared_2',['ConsumerKeyShared',['../namespacepulsar.html#a9001dd59f719a974dc7b4ef5858dda8eacb902cf3781caf307d06e37dc447d5cc',1,'pulsar']]],
+  ['consumershared_3',['ConsumerShared',['../namespacepulsar.html#a9001dd59f719a974dc7b4ef5858dda8eac55370821e835a03c2da742ab27e1705',1,'pulsar']]]
+];
---
+++ static/api/cpp/4.2.x/search/enumvalues_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['separated_0',['SEPARATED',['../namespacepulsar.html#a37a42c28f591ba599466990fb42e04a6a561af55a6a824ae74125a6cf4eade06a',1,'pulsar']]],
+  ['shared_1',['Shared',['../classpulsar_1_1_producer_configuration.html#ad8fa778cac0085456ddee943e19d4992a73f8f203fbd16f91a5b123a362bf13e5',1,'pulsar::ProducerConfiguration']]],
+  ['sticky_2',['STICKY',['../namespacepulsar.html#adaab2afae42f06da675d68501dd253deaed98d14aabee0f3bc70a0cc7723e5b16',1,'pulsar']]],
+  ['string_3',['STRING',['../namespacepulsar.html#a110c1992e36a25acc192ea4f399bc4f8ab936a9dad2cbb5fca28fc477ff39fb70',1,'pulsar']]]
+];
