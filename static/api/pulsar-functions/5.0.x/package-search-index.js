d7def4897b7e186a0887fdcde3121b36015e0047
Lari Hotari <lhotari@users.noreply.github.com>
2026-09-17 02:35:17 +0300

    update java-apidoc for 5.0.0-M2

--- static/api/pulsar-functions/5.0.x/package-search-index.js
+++ static/api/pulsar-functions/5.0.x/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.pulsar.functions.api"},{"l":"org.apache.pulsar.functions.api.state"},{"l":"org.apache.pulsar.functions.api.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.apache.pulsar.functions.api"},{"l":"org.apache.pulsar.functions.api.state"},{"l":"org.apache.pulsar.functions.api.utils"}];updateSearchResults();
